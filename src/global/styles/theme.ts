85ccbd92d48b363446f7773107c9cbb5b4f06f8e
AnthonySa <anthonysareis11@gmail.com>
2024-07-05 12:43:48 -0300

    feat: configure theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    darkBackground: "#031314",
+    secondaryBackground: "#052224",
+    primaryText: "#093030",
+    secondaryText: "#0E3E3E",
+    primaryGreen: "#00D09E",
+    lightGreen: "#DFF7E2",
+    lightBackground: "#F1FFF3",
+    lightBlue: "#6DB6FE",
+    primaryBlue: "#3299FF",
+    oceanBlue: "#0068FF",
+  },
+};
+
+export default theme;
